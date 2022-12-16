8d00ecc34cb91f66e86d9c95e9cce5413818a724
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-12-16 15:21:05 -0500

    desogned ui  and added functions

---
+++ script.js
@@ -0,0 +1,66 @@
+var origBoard;
+
+const huPlayer = "O";
+const aiPlayer = "X";
+
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+const cells = document.querySelectorAll(".cell");
+
+startGame();
+
+function startGame() {
+  document.querySelector(".endgame").style.display = "none";
+  origBoard = Array.from(Array(9).keys());
+
+  for (let index = 0; index < cells.length; index++) {
+    cells[index].innerText = "";
+    cells[index].style.removeProperty("background-color");
+
+    cells[index].addEventListener("click", turnClick, false);
+  }
+}
+function turnClick(square) {
+  turn(square.target.id, huPlayer);
+}
+
+function turn(squareID, player) {
+  origBoard[squareID] = player;
+  document.getElementById(squareID).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+
+  if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == huPlayer ? "blue" : "red";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
+  declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+}
